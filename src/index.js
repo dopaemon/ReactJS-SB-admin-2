63284d8b7b52ceea767189a3b1118030af95d47a
dopaemon <polarisdp@gmail.com>
2023-10-01 21:06:53 +0700

    src: Import initial library from SBAdmin2
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// SBAdmin 2
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './assets/scss/sb-admin-2.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
