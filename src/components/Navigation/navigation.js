315fd17faaef90cba8fa2774cd1c1d6734aac629
dopaemon <polarisdp@gmail.com>
2023-10-01 23:31:47 +0700

    src: Initial Component
    Signed-off-by: dopaemon <polarisdp@gmail.com>

---
+++ src/components/Navigation/navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SiderBar from "./SiderBar/index.js";
+import TopBar from "./TopBar/index.js";
+
+function SBNavigation() {
+    return (
+        <>
+            <TopBar />
+            <SiderBar />
+        </>
+    )
+}
+
+export default SBNavigation;
